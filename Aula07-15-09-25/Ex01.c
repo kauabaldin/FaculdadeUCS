3df9a745eea01e439ffedd5300c8fb0f3a3cf10d
Kaua Morera Baldin <167149091+kauabaldin@users.noreply.github.com>
2025-09-25 14:30:39 +0000

    Add

--- Aula07-15-09-25/Ex01.c
+++ Aula07-15-09-25/Ex01.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 int main() {
-    
+    int number, negativo;
+    printf("Digite um numero: ");
+    scanf("%d", &number);
+    while (number != 0) {
+        printf("\nDigite um numero: ");
+        scanf("%d", &number);
+        if (number == 0) {
+            break;
+        } else if (number < 0) {
+            number = negativo; 
+            continue;
+        }
+        printf("O numero menor numero digitado: %d", number);
+    }
+    return 0;
 }
